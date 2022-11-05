9bd2b2ff8e3fffcb9e85ddf6a38fa6a2a5bd06fd
Bogdan Nikitin <skopthe6@gmail.com>
2022-11-05 21:14:24 +0300

    feat: add eslint, prettier

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,18 +1,27 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true,
   },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ["plugin:vue/vue3-recommended", "eslint:recommended", "prettier"],
+  plugins: ["prettier", "vue"],
   parserOptions: {
     parser: "@babel/eslint-parser",
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": 1,
+    "no-console": 1,
+    "no-debugger": 1,
+    "no-useless-catch": 0,
+    "no-empty": 0,
+    "eol-last": [2, "always"],
+
+    indent: ["error", 2],
+    "vue/html-indent": ["error", 2],
+    "vue/singleline-html-element-content-newline": 0,
+    "vue/component-name-in-template-casing": ["error", "PascalCase"],
+    "vue/attribute-hyphenation": 0,
+    "vue/v-on-event-hyphenation": 0,
   },
 };
