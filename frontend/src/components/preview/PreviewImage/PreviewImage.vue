ab8a006427ecd6c453fccc1dafd70e91a4562249
Bogdan Nikitin <skopthe6@gmail.com>
2022-11-16 07:30:06 +0300

    feat: add basic constructor preview components

---
+++ frontend/src/components/preview/PreviewImage/PreviewImage.vue
@@ -0,0 +1,27 @@
+<template>
+  <img
+    class="preview-image"
+    :src="imageUrl"
+    :alt="props.alt"
+  />
+</template>
+
+<script setup>
+import { defineProps, computed } from "vue";
+import imageSrc from "@/helpers/imageSrc";
+
+const props = defineProps({
+  url: {
+    type: String,
+    required: true,
+  },
+  alt: {
+    type: String,
+    required: true,
+  },
+});
+
+const imageUrl = computed(() => imageSrc(props.url));
+</script>
+
+<style lang="scss" scoped></style>
--- frontend/src/components/type/TypeConstructor/TypeConstructor.vue
+++ frontend/src/components/type/TypeConstructor/TypeConstructor.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="type-constructor">
     <h1 class="type-constructor__title">{{ store.getters.typeData.attributes.title }}</h1>
-    <div class="constructor"></div>
+    <div class="constructor">
+      <ConstructorPreview :baseImage="store.getters.typeData.attributes.base.data.attributes" />
+    </div>
   </div>
 </template>
 
 <script setup>
 import { useStore } from "vuex";
 import { defineProps } from "vue";
+
+import ConstructorPreview from "@/components/preview/ConstructorPreview/ConstructorPreview.vue";
 
 const props = defineProps({
   typeId: {
@@ -20,4 +24,13 @@ const store = useStore();
 await store.dispatch("fetchType", props.typeId);
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.type-constructor {
+  display: flex;
+  flex-direction: column;
+
+  &__title {
+    margin-bottom: 24px;
+  }
+}
+</style>
